5f3c63474f169efe6caa81f3c8dc044887554ae5
Alex Strilets <astrilet@gmail.com>
2015-10-08 16:01:17 -0600

    added tmux pluigin

--- mklnk.sh
+++ mklnk.sh
@@ -4,31 +4,36 @@ cd ~
 
 if [ -f .bash_profile -a ! -L .bash_profile ]; then
   mv .bash_profile .bash_profile.save
+  ln -s ./configs/bash_profile .bash_profile
 fi
 
 if [ -f .bashrc -a ! -L .bashrc ]; then
   mv .bashrc .bashrc.save
+  ln -s ./configs/bashrc .bashrc
 fi
 
 if [ -f .vimrc -a ! -L .vimrc ]; then
   mv .vimrc .vimrc.save
+  ln -s ./configs/vimrc .vimrc
 fi
 
 if [ -d .vim -a ! -L .vim ]; then
   mv .vim .vim.save
+  ln -s ./configs/vim ./.vim
+fi
+
+if [ -d .tmux -a ! -L .tmux ]; then
+  mv .tmux .tmux.save
+  ln -s ./configs/tmux ./.tmux
 fi
 
 if [ -d .tmux.conf -a ! -L .tmux.conf ]; then
   mv .tmux.conf .tmux.conf.save
+  ln -s ./configs/tmux.conf .tmux.conf
 fi
 
 if [ -f .aliases -a ! -L .aliases ]; then
   mv .aliases .aliases.save
+  ln -s ./configs/aliases .aliases
 fi
 
-ln -s ./configs/bash_profile .bash_profile
-ln -s ./configs/bashrc .bashrc
-ln -s ./configs/vimrc .vimrc
-ln -s ./configs/vim ./.vim
-ln -s ./configs/aliases .aliases
-ln -s ./configs/tmux.conf .tmux.conf
